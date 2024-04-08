2109646eacacf9f89a3343e961d2ec943d6cde49
angeloguiao <angeloguiao.gci@gmail.com>
2024-04-08 14:22:16 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+import React from 'react';
+function App() {
+    return (
+        <div className='bg-gray-500'>
+          
+        </div>
+    )
+}
+
+export default App;
